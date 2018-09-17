3b2dd8eaf94b1529d7ff1fbcbf3c9917ad2577ca
newlifebegins <1345795498@qq.com>
2018-09-17 17:49:20 +0800

    init

--- routers/admin.js
+++ routers/admin.js
@@ -1,5 +1,7 @@
 // TODO: 加载express模块
 const express = require('express');
+const formidable = require('formidable');
+const fs = require('fs');
 const router = express.Router();
 const User = require('../models/Users');
 const Category = require('../models/Categories');
@@ -264,6 +266,51 @@ router.post('/content/add', (req, res, next) => {
     let contitle = req.body.contitle;
     let condesc = req.body.condesc;
     let content = req.body.content;
+    //创建上传表单
+    var form = new formidable.IncomingForm();
+    //设置编辑
+    form.encoding = 'utf-8';
+    //设置上传目录
+    form.uploadDir = 'public/avatar/';
+    //保留后缀
+    form.keepExtensions = true;
+    //文件大小 2M
+    form.maxFieldsSize = 2 * 1024 * 1024;
+    // 上传文件的入口文件
+    form.parse(req, function(err, fields, files) {
+        console.log(files);
+
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        var extName = '';  //后缀名
+        switch (files.fulAvatar.type) {
+            case 'image/pjpeg':
+                extName = 'jpg';
+                break;
+            case 'image/jpeg':
+                extName = 'jpg';
+                break;
+            case 'image/png':
+                extName = 'png';
+                break;
+            case 'image/x-png':
+                extName = 'png';
+                break;
+        }
+
+        if(extName.length == 0) {
+            console.log('只支持png和jpg格式图片');
+            return;
+        }
+
+        var avatarName = Math.random() + '.' + extName;
+        var newPath = form.uploadDir + avatarName;
+        fs.renameSync(files.fulAvatar.path, newPath);  //重命名
+    });
+
     if(!category) {
         res.render('admin/error', {
             message: '内容分类不能为空'
